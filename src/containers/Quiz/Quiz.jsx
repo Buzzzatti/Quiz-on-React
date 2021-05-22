0b58dd5a5a6148b5c11a48d1a4d24303b55c7ec7
Buzzzatti <rbuzaev1@gmail.com>
2021-05-22 21:17:34 +0400

    Quiz Ready

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 import classes from './Quiz.module.css'
-import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 
 class Quiz extends Component {
-
-    state={
+    state = {
+        results: {}, // {[id]: success error}
+        isFinished: false,
         activeQuestion: 0,
-        answerState: null,
+        answerState: null, // { [id]: 'success' 'error' }
         quiz: [
             {
-                question:'Какого цвета небо ?',
+                question: 'Какого цвета небо?',
                 rightAnswerId: 2,
                 id: 1,
                 answers: [
@@ -20,43 +22,58 @@ class Quiz extends Component {
                 ]
             },
             {
-                question:'В каком году это вот ',
-                rightAnswerId: 4,
+                question: 'В каком году основали Санкт-Петербург?',
+                rightAnswerId: 3,
                 id: 2,
                 answers: [
-                    {text: '12', id: 1},
-                    {text: '2012', id: 2},
-                    {text: '2021', id: 3},
-                    {text: '2022', id: 4}
+                    {text: '1700', id: 1},
+                    {text: '1702', id: 2},
+                    {text: '1703', id: 3},
+                    {text: '1803', id: 4}
                 ]
             }
         ]
     }
 
-    onAnswerClickHandler = (answerId) =>{
-        const question = this.state.quiz[this.state.activeQuestion]
+    onAnswerClickHandler = answerId => {
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (this.state.answerState[key] === 'success') {
+                return
+            }
+        }
 
-        if(question.rightAnswerId === answerId) {
+        const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
 
             this.setState({
-                answerState : {[answerId]: 'success'}
+                answerState: {[answerId]: 'success'},
+                results
             })
 
             const timeout = window.setTimeout(() => {
-                if(this.isQuizFinished()) {
-                    console.log('Finished')
-                }else {
+                if (this.isQuizFinished()) {
+                    this.setState({
+                        isFinished: true
+                    })
+                } else {
                     this.setState({
                         activeQuestion: this.state.activeQuestion + 1,
                         answerState: null
                     })
                 }
-
                 window.clearTimeout(timeout)
-            },1000)
-        }else{
+            }, 1000)
+        } else {
+            results[question.id] = 'error'
             this.setState({
-                answerState: {[answerId]: 'error'}
+                answerState: {[answerId]: 'error'},
+                results
             })
         }
     }
@@ -65,24 +82,42 @@ class Quiz extends Component {
         return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
-    render() {
-        return(
-            <div className={classes.Quiz}>
-               <div className={classes.QuizWrapper}>
-                   <h1>Ответьте на все вопросы</h1>
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {}
+        })
+    }
 
-                    <ActiveQuiz
-                        onAnswerClick={this.onAnswerClickHandler}
-                        answers={this.state.quiz[this.state.activeQuestion].answers}
-                        question={this.state.quiz[this.state.activeQuestion].question}
-                        quizLength={this.state.quiz.length}
-                        answerNumber={this.state.activeQuestion + 1}
-                        state={this.state.answerState}
-                    />
+    render() {
+        return (
+            <div className={classes.Quiz}>
+                <div className={classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы</h1>
+
+                    {
+                        this.state.isFinished
+                            ? <FinishedQuiz
+                                results={this.state.results}
+                                quiz={this.state.quiz}
+                                onRetry={this.retryHandler}
+                            />
+                            : <ActiveQuiz
+                                answers={this.state.quiz[this.state.activeQuestion].answers}
+                                question={this.state.quiz[this.state.activeQuestion].question}
+                                onAnswerClick={this.onAnswerClickHandler}
+                                quizLength={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion + 1}
+                                state={this.state.answerState}
+                            />
+                    }
                 </div>
             </div>
         )
     }
 }
+
 
 export default Quiz
